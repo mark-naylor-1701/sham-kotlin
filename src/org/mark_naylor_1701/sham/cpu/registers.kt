b728349c304fd1f1f463c8635060d5c2be427050
Mark W. Naylor <mark.naylor.1701@gmail.com>
2020-09-05 16:34:11 -0400

    Registers
    Add support for register names, codes, and relationships between names
    and codes.

--- src/org/mark_naylor_1701/sham/cpu/registers.kt
+++ src/org/mark_naylor_1701/sham/cpu/registers.kt
@@ -4,12 +4,94 @@
 
 package org.mark_naylor_1701.sham.cpu
 
+import org.mark_naylor_1701.sham.Types.*
 
-data class Name private constructor(val value: String) {
+
+data class RegisterName private constructor(val value: String) {
     companion object {
-        fun ctor(name: String) = Name(name)
+        fun ctor(name: String): RegisterName {
+            val fixedName: String = name.toLowerCase()
+            return when (fixedName) {
+                in legalRegisterNames -> RegisterName(fixedName)
+                else -> throw RegisterNameException("$name is not a legal register name.")
+            }
+        }
     }
 }
+
+data class RegisterCode private constructor (val value: Byte) {
+    companion object {
+        fun ctor(sb: ShamByte): RegisterCode = when (sb.value) {
+            in (0 until legalRegisterNames.size) -> RegisterCode(sb.value)
+            else -> throw RegisterCodeException("$sb.value outside legal register code fun.")
+        }
+
+        fun ctor(n: Number): RegisterCode = when (n) {
+            in (0 until legalRegisterNames.size) -> RegisterCode(n.toByte())
+            else -> throw RegisterCodeException("$n.value outside legal register code fun.")
+        }
+    }
+}
+
+
+
+class RegisterNameException(msg: String): Exception(msg)
+class RegisterCodeException(msg: String): Exception(msg)
+class BranchNameException(msg: String): Exception(msg)
+
+private val legalRegisterNames: List<String> = listOf (
+    "nx", "ax", "bx", "cx", "dx", "ex", "fx", "dr", "ip", "sr", "sp", "fr"
+)
+
+private val nameCode : Map<RegisterName, RegisterCode> =
+    legalRegisterNames.withIndex().map {
+        RegisterName.ctor(it.value) to RegisterCode.ctor(ShamByte(it.index.toByte()))
+    }.toMap()
+
+
+private val codeName: Map<RegisterCode, RegisterName> =
+    nameCode.map { it.value to it.key }.toMap()
+
+fun registerName(code: RegisterCode): RegisterName? = codeName[code]
+
+fun registerCode(name: RegisterName): RegisterCode? = nameCode[name]
+
+
+data class BranchName private constructor (val value: String) {
+    companion object {
+        fun ctor(name: String): BranchName {
+            val fixedName = name.toLowerCase()
+            return when (fixedName) {
+                in legalBranchNames -> BranchName(fixedName)
+                else -> throw BranchNameException("$name is not a legal branch name.")
+            }
+        }
+    }
+}
+
+data class BranchCode private constructor (val value: Byte) {
+    companion object {
+        fun ctor(sb: ShamNybble): BranchCode {
+            return BranchCode(sb.value)
+        }
+
+        fun ctor(n: Number): BranchCode {
+            return BranchCode(ShamNybble.ctor(n).value)
+        }
+    }
+}
+
+private val legalBranchNames = listOf("low", "equal", "high", "key")
+
+private val branchNameCode: Map<BranchName, BranchCode> =
+    legalBranchNames.withIndex().
+    map { BranchName.ctor(it.value) to BranchCode.ctor(it.index)}.toMap()
+
+private val branchCodeName: Map<BranchCode, BranchName> =
+    branchNameCode.map { it.value to it.key }.toMap()
+
+fun branchCode(name: BranchName): BranchCode? = branchNameCode[name]
+fun barnchName(code: BranchCode): BranchName? = branchCodeName[code]
 
 // ------------------------------------------------------------------------------
 // BSD 3-Clause License
@@ -41,4 +123,3 @@ data class Name private constructor(val value: String) {
 // CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
