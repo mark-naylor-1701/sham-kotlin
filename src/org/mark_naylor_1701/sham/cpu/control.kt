4d4b5b45e1a4351c5ad2baabfa1124503a186027
Mark W. Naylor <mark.naylor.1701@gmail.com>
2020-09-14 14:16:35 -0400

    Add control module
    Contains and manages current command and interrupt "circuits",

--- src/org/mark_naylor_1701/sham/cpu/control.kt
+++ src/org/mark_naylor_1701/sham/cpu/control.kt
@@ -1,55 +1,20 @@
 // author: Mark W. Naylor
-// file:  memory.kt
-// date:  2020-Sep-08
+// file:  control.kt
+// date:  2020-Sep-14
+
+// Represents part of the control circuitry: the current command, the
+// interrupt enable flag, and the current interrupt clock value.
 
 package org.mark_naylor_1701.sham.cpu
 
 import org.mark_naylor_1701.sham.Types.*
 
-
-// Final memory size, reduced during development/testing.
-// private val memorySize = 32 * 1024
-
-// Temporary memory size, 64 bytes should be sufficient
-private val memorySize = 64
-
-typealias Memory = List<ShamByte>
-
-operator fun Memory.get(index: ShamWord): ShamByte = get(index.value.toInt())
-
-fun Memory.take(index: ShamWord): Memory = take(index.value.toInt())
-
-fun Memory.drop(index: ShamWord): Memory = drop(index.value.toInt())
-
-fun newMemory(): Memory {
-    val zero = ShamByte(0)
-
-    return (1..memorySize).map { zero }
-}
-
-fun readByte(mem: Memory, address: ShamWord): ShamByte {
-    return mem[address]
-}
-
-fun readWord(mem: Memory, address: ShamWord): ShamWord {
-    val hiByte = readByte(mem, address)
-    val loByte = readByte(mem, address + ShamWord(1))
-    return shamWord(hiByte, loByte)
-}
-
-fun write(mem: Memory, address: ShamWord, byte: ShamByte): Memory {
-    val pre = mem.take(address)
-    val post = mem.drop(address.inc())
-
-    return pre + byte + post
-}
-
-fun write(mem: Memory, address: ShamWord, word: ShamWord): Memory {
-    val pre = mem.take(address)
-    val post = mem.drop(address.inc().inc())
-    val (hi, lo) = halves(word)
-
-    return pre + hi + lo + post
+data class Control(val command: OpCode = OpCode(0),
+                   val enabled: Boolean = false,
+                   val limit: ShamWord = ShamWord(0)) {
+    fun next(next: OpCode) = copy(command=next)
+    fun enable(clockValue: ShamWord) = copy(enabled=true, limit=clockValue)
+    fun disable() = copy(enabled=false, limit=ShamWord(0))
 }
 
 
